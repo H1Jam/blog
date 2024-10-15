d8d6f69014186a7605eaa4a899cb2175d74fac10
H1Jam <46633362+H1Jam@users.noreply.github.com>
2024-10-14 23:20:34 -0400

    update page style

--- script.js
+++ script.js
@@ -6,12 +6,34 @@ function fetchPersonsData(url) {
         .then(data => {
             console.log('posts:', data);
             data.forEach(post => {
-                apiResultDisplay.innerHTML += `<div class="blog-posts"><div class="blog-posts-header">${post.title.rendered}</div>${post.excerpt.rendered}<div>${post.date}</div></div>`;
+                apiResultDisplay.innerHTML += 
+                `<div class="blog-posts">
+                    <div class="blog-posts-header">
+                        ${post.title.rendered}
+                    </div>
+                    <div class="blog-posts-content">
+                        ${post.excerpt.rendered}
+                    </div>
+                    <div class="blog-posts-footer">
+                        ${formatDate(post.date)}
+                    </div>
+                </div>`;
             });
         })
         .catch(error => console.error('Error fetching data:', error));
 }
 
-const dataUrl = 'https://hjam.ca/wp-json/wp/v2/posts?per_page=10&context=embed';
+// api gives us something like this "2021-04-02T13:53:23" 
+// but we want to display it like this "April 2, 2021" 
+// so we use this function to format the date.
+function formatDate(dateString) {
+    return new Date(dateString).toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    });
+}
+
+const dataUrl = 'https://hjam.ca/wp-json/wp/v2/posts';
 
 fetchPersonsData(dataUrl);
