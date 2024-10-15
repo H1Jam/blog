bd098eb1b4515be411e4ff42d54b310790b4ed19
H1Jam <46633362+H1Jam@users.noreply.github.com>
2024-10-14 20:28:14 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const apiResultDisplay = document.getElementById('posts');
+
+function fetchPersonsData(url) {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log('posts:', data);
+            data.forEach(post => {
+                apiResultDisplay.innerHTML += `<div class="blog-posts"><b>${post.title.rendered}</b><p>${post.excerpt.rendered}</p></div>`;
+            });
+        })
+        .catch(error => console.error('Error fetching data:', error));
+}
+
+// Example URL (replace with your actual URL)
+const dataUrl = 'https://hjam.ca/wp-json/wp/v2/posts?per_page=10&context=embed';
+
+// Fetch data when the script loads
+fetchPersonsData(dataUrl);
